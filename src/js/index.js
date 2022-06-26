6016faa8f95d74413a79f62523e2fd7fa12cc745
weiwenyu <2454150875@qq.com>
2022-06-26 15:05:14 +0800

    提交按钮\小圆点\代码区域

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,16 @@
  * @Author: wwy
  * @Date: 2022-06-23 17:48:42
  * @LastEditors: wwy
- * @LastEditTime: 2022-06-23 18:01:44
+ * @LastEditTime: 2022-06-26 14:23:16
  */
 import "../style/index.scss";
+
+const dark = document.getElementById("dark");
+console.log(dark);
+dark.addEventListener("click", function () {
+  if (window.document.documentElement.getAttribute("data-theme") === "dark") {
+    window.document.documentElement.setAttribute("data-theme", "light");
+  } else {
+    window.document.documentElement.setAttribute("data-theme", "dark");
+  }
+});
